b90d19319c33779dba702888dce2b16df6b40318
Alexander Melian <alexandermelian@outlook.com>
2021-05-27 13:57:26 -0300

    First commit

---
+++ src/main/java/com/alkemy/DisneyAPI/DisneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.DisneyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkemy/DisneyAPI/DisneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.DisneyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
